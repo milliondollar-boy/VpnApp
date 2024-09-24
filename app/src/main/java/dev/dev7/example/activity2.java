cf977fd4b4f9ba7cc4d1e9fede76e0775cec074a
Denis Mironchenko <mironchenko.d.g@gmail.com>
2024-09-24 16:49:14 +0300

    new button: sbros, tg. LinearLayout -> ConstraintLayout

--- app/src/main/java/dev/dev7/example/activity2.java
+++ app/src/main/java/dev/dev7/example/activity2.java
@@ -1,6 +1,9 @@
 package dev.dev7.example;
 
+import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.content.SharedPreferences;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,15 @@ import dev.dev7.lib.v2ray.utils.V2rayConstants;
 public class activity2 extends AppCompatActivity {
 
     private ImageButton connect;
+    private Button reset, tg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         SharedPreferences sharedPreferences;
+        sharedPreferences = getSharedPreferences("conf", MODE_PRIVATE);
+
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
@@ -23,15 +30,18 @@ public class activity2 extends AppCompatActivity {
         if (savedInstanceState == null)
         {
             connect = findViewById(R.id.btn_connect_image);
+            reset = findViewById(R.id.button6);
+            tg = findViewById(R.id.button7);
         }
 
-        sharedPreferences = getSharedPreferences("conf", MODE_PRIVATE);
+        reset.setOnClickListener(v -> resetTheConfiguration());
 
-        // Чтение сохранённой конфигурации
-        String v2ray_config = sharedPreferences.getString("v2ray_config", "");
+        tg.setOnClickListener(v -> openTelegramBot());
 
         connect.setOnClickListener(view ->
         {
+            // Чтение сохранённой конфигурации
+            String v2ray_config = sharedPreferences.getString("v2ray_config", "");
             if (V2rayController.getConnectionState() == V2rayConstants.CONNECTION_STATES.DISCONNECTED) {
                 V2rayController.startV2ray(this, "Test Server", v2ray_config, null);
                 connect.setImageResource(R.drawable.new_photo);
@@ -40,5 +50,18 @@ public class activity2 extends AppCompatActivity {
                 connect.setImageResource(R.drawable.photo);
             }
         });
+    }
+
+    public void openTelegramBot(){
+        String botUsername = "My First Bot"; // Замените на имя вашего бота
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://t.me/crackeppp_bot" + botUsername));
+        startActivity(intent);
+    }
+
+    public void resetTheConfiguration(){
+        // v2ray_config.getDefaultConfig();
+        Intent intent = new Intent(activity2.this, activity1.class);
+        startActivity(intent);
     }
 }
