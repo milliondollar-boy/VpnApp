ce35ee0bbc1f85c10e6a2d611742781ad7744516
Denis Mironchenko <mironchenko.d.g@gmail.com>
2024-09-24 17:33:48 +0300

    Starting point

--- app/src/main/java/dev/dev7/example/activity2.java
+++ app/src/main/java/dev/dev7/example/activity2.java
@@ -1,20 +1,45 @@
 package dev.dev7.example;
 
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_CONNECTION_STATE_BROADCAST_EXTRA;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_DOWNLOAD_SPEED_BROADCAST_EXTRA;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_DOWNLOAD_TRAFFIC_BROADCAST_EXTRA;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_DURATION_BROADCAST_EXTRA;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_UPLOAD_SPEED_BROADCAST_EXTRA;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_UPLOAD_TRAFFIC_BROADCAST_EXTRA;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.V2RAY_SERVICE_STATICS_BROADCAST_INTENT;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.content.SharedPreferences;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
 import dev.dev7.lib.v2ray.V2rayController;
+import dev.dev7.lib.v2ray.utils.V2rayConfigs;
 import dev.dev7.lib.v2ray.utils.V2rayConstants;
 
 public class activity2 extends AppCompatActivity {
 
     private ImageButton connect;
-    private Button reset, tg;
+    private Button reset, tg, connection;
+    private TextView connection_speed, connection_traffic, connection_time, server_delay, connected_server_delay, connection_mode;
+    private BroadcastReceiver v2rayBroadCastReceiver;
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,16 +57,25 @@ public class activity2 extends AppCompatActivity {
             connect = findViewById(R.id.btn_connect_image);
             reset = findViewById(R.id.button6);
             tg = findViewById(R.id.button7);
+            V2rayController.init(this, R.drawable.ic_launcher, "V2ray Android");
+            V2rayController.init(this, R.drawable.ic_launcher, "V2ray Android");
+            connection_speed = findViewById(R.id.connection_speed);
+            connection_time = findViewById(R.id.connection_duration);
+            connection_traffic = findViewById(R.id.connection_traffic);
+            server_delay = findViewById(R.id.server_delay);
+            connection_mode = findViewById(R.id.connection_mode);
+            connected_server_delay = findViewById(R.id.connected_server_delay);
+            connection = findViewById(R.id.button15);
         }
 
         reset.setOnClickListener(v -> resetTheConfiguration());
 
         tg.setOnClickListener(v -> openTelegramBot());
-
+        String v2ray_config = sharedPreferences.getString("v2ray_config", "");
         connect.setOnClickListener(view ->
         {
             // Чтение сохранённой конфигурации
-            String v2ray_config = sharedPreferences.getString("v2ray_config", "");
+
             if (V2rayController.getConnectionState() == V2rayConstants.CONNECTION_STATES.DISCONNECTED) {
                 V2rayController.startV2ray(this, "Test Server", v2ray_config, null);
                 connect.setImageResource(R.drawable.new_photo);
@@ -50,6 +84,56 @@ public class activity2 extends AppCompatActivity {
                 connect.setImageResource(R.drawable.photo);
             }
         });
+        connected_server_delay.setOnClickListener(view -> {
+            connected_server_delay.setText("connected server delay : measuring...");
+            // Don`t forget to do ui jobs in ui thread!
+            V2rayController.getConnectedV2rayServerDelay(this, delayResult -> runOnUiThread(() -> connected_server_delay.setText("connected server delay : " + delayResult + "ms")));
+        });
+        // Another way to check the connection delay of a config without connecting to it.
+        server_delay.setOnClickListener(view -> {
+            server_delay.setText("server delay : measuring...");
+            new Handler().postDelayed(() -> server_delay.setText("server delay : " + V2rayController.getV2rayServerDelay(v2ray_config) + "ms"), 200);
+        });
+
+        connection_mode.setOnClickListener(view -> {
+            V2rayController.toggleConnectionMode();
+            connection_mode.setText("connection mode : " + V2rayConfigs.serviceMode.toString());
+        });
+
+
+        v2rayBroadCastReceiver = new BroadcastReceiver() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                runOnUiThread(() -> {
+                    connection_time.setText("connection time : " + Objects.requireNonNull(intent.getExtras()).getString(SERVICE_DURATION_BROADCAST_EXTRA));
+                    connection_speed.setText("connection speed : " + intent.getExtras().getString(SERVICE_UPLOAD_SPEED_BROADCAST_EXTRA) + " | " + intent.getExtras().getString(SERVICE_DOWNLOAD_SPEED_BROADCAST_EXTRA));
+                    connection_traffic.setText("connection traffic : " + intent.getExtras().getString(SERVICE_UPLOAD_TRAFFIC_BROADCAST_EXTRA) + " | " + intent.getExtras().getString(SERVICE_DOWNLOAD_TRAFFIC_BROADCAST_EXTRA));
+                    connection_mode.setText("connection mode : " + V2rayConfigs.serviceMode.toString());
+                    switch ((V2rayConstants.CONNECTION_STATES) Objects.requireNonNull(intent.getExtras().getSerializable(SERVICE_CONNECTION_STATE_BROADCAST_EXTRA))) {
+                        case CONNECTED:
+                            connection.setText("CONNECTED");
+                            break;
+                        case DISCONNECTED:
+                            connection.setText("DISCONNECTED");
+                            connected_server_delay.setText("connected server delay : wait for connection");
+                            break;
+                        case CONNECTING:
+                            connection.setText("CONNECTING");
+                            break;
+                        default:
+                            break;
+                    }
+                });
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(v2rayBroadCastReceiver, new IntentFilter(V2RAY_SERVICE_STATICS_BROADCAST_INTENT), RECEIVER_EXPORTED);
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                registerReceiver(v2rayBroadCastReceiver, new IntentFilter(V2RAY_SERVICE_STATICS_BROADCAST_INTENT), Context.RECEIVER_NOT_EXPORTED);
+            }
+        }
     }
 
     public void openTelegramBot(){
@@ -63,5 +147,12 @@ public class activity2 extends AppCompatActivity {
         // v2ray_config.getDefaultConfig();
         Intent intent = new Intent(activity2.this, activity1.class);
         startActivity(intent);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (v2rayBroadCastReceiver != null){
+            unregisterReceiver(v2rayBroadCastReceiver);
+        }
     }
 }
